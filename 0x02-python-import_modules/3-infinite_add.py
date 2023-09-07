e254ce764c24e209af1d1b2da0f2737a87ec7a3f
FahdAzouz <fahdstudies00@gmail.com>
2023-09-07 12:53:01 +0100

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     import sys
     tot = 0
-    i = 0
-    for arg in sys.argv:
+    for i in range(len(sys.argv) - 1):
         tot += int(sys.argv[i + 1])
-        i += 1
     print("{:d}".format(tot))
