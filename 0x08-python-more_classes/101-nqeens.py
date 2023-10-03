76709ec7ce07f89a7040ca2ea92e3f199fcad0ec
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-10-03 18:35:20 +0100

    Update 101-nqeens.py

--- 0x08-python-more_classes/101-nqeens.py
+++ 0x08-python-more_classes/101-nqeens.py
@@ -8,33 +8,6 @@ queen must be placed on the chessboard.
 """
 import sys
 
-
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return (board)
-
-
-def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return (board)
-
-
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return (solution)
-
-
 def xout(board, row, col):
     """X out spots on a chessboard.
     All spots where non-attacking queens can no
@@ -84,6 +57,31 @@ def xout(board, row, col):
             break
         board[r][c] = "x"
         c -= 1
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
+
 
 
 def recursive_solve(board, row, queens, solutions):
