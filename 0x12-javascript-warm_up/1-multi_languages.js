d838f4d1aed7cf6a8a46635d262159b30e2fadd1
FahdAzouz <fahdstudies00@gmail.com>
2023-12-11 15:48:37 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,2 @@
-#!/usr/bin/bash
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+#!/usr/bin/node
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
