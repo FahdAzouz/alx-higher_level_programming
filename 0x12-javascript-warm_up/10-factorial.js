857118e2563cbaaadc6cc8d5a99b518ea4de14e1
FahdAzouz <fahdstudies00@gmail.com>
2023-12-11 18:21:08 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-function factorial (x) {
-	if (isNaN(x)) {
-		return 1;
-	}
-	else {
-		let result = 1;
-		for (let i = 1; i === x; i++) {
-			result *= i;
-		}
-		return result;
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
 console.log(factorial(Number(process.argv[2])));
