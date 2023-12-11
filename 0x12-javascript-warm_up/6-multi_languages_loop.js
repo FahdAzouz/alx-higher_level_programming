b224b7f1944d2cf3d5e317603d623efda2768c2c
FahdAzouz <fahdstudies00@gmail.com>
2023-12-11 17:43:56 +0100

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const item in arr) {
-	console.log(item);
-};
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
+}
