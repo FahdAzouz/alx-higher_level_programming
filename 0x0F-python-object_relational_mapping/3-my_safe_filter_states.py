10a4f3aa78e299e51a2c63b4e84dc0cb6211cda9
FahdAzouz <fahdstudies00@gmail.com>
2023-12-14 14:49:07 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def filter_states(username, password, db_name, stte_name):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    # Execute the query to get states starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE BINARY '%s'"
+    param = ("N%",)
+    cursor.execute(query, param)
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Display results
+    for state in states:
+        print(state)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name, state_name = sys.argv[1:5]
+    filter_states(username, password, db_name, state_name)
+
