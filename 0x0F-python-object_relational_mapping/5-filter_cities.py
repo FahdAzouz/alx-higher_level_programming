18a3244005ae5b09d09643ae45b0c58f46639f42
FahdAzouz <fahdstudies00@gmail.com>
2023-12-15 15:23:53 +0100

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
-''' Search cities of a state in a database '''                
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def filter_states(username, password, db_name, stte_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Execute the query to get states starting with 'N'
-    query = "SELECT cities.name FROM states INNER JOIN cities ON cities.state_id = (SELECT id FROM states WHERE states.name LIKE BINARY '{}')".format(stte_name)
-    cursor.execute(query)
-
-    # Fetch all the rows
-    cities = cursor.fetchall()
-    # Display results
-    i = 1;
-    for city in cities:
-        if (i == len(cities)):
-            print(city)
-        else:
-            print("{}, ".format(city), end="")
-            i+=1
-
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, db_name, state_name = sys.argv[1:5]
-    filter_states(username, password, db_name, state_name)
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
