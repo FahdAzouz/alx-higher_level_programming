1eb40653b58153323cfee717696735e9f14f10cb
FahdAzouz <fahdstudies00@gmail.com>
2023-12-15 15:18:27 +0100

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-""" list all cities with their states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def filter_states(username, password, db_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Execute the query to get states starting with 'N'
-    query = "SELECT cities.id, cities.name, state.name FROM cities INNER JOIN states ON states.id=cities.state_id ORDERED by cities.id ASC"
-    cursor.execute(query)
-
-    # Fetch all the rows
-    states = cursor.fetchall()4-cities_by_state.py
-
-    # Display results
-    for state in states:
-        print(state)
-
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, db_name = sys.argv[1:4]
-    filter_states(username, password, db_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
