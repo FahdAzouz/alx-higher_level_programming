dd0ac23519dd088f220da6dfa27a19676d9cdbcc
FahdAzouz <fahdstudies00@gmail.com>
2023-12-17 15:07:07 +0100

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-""" Script that outputs the state """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-def filter_states(username, password, db_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
-
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Execute the query to get states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    # Fetch all the rows
-    states = cursor.fetchall()
-
-    # Display results
-    for state in states:
-        print(state)
-
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, db_name = sys.argv[1:4]
-    filter_states(username, password, db_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
