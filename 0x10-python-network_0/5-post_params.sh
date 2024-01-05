d1d5d8db2449550c50be127466bc2d6a195d22f2
FahdAzouz <fahdstudies00@gmail.com>
2024-01-05 21:44:08 +0100

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,4 @@
 # sends a GET request to the URL, and displays the body of the response
 var1="test@gmail.com"
 var2="I will always be here for PLD"
-curl -s -X POST -d "email=$var1&subject=$var2" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
