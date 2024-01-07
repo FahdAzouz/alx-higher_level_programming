c7e3917cebaa99d1c7f641c71b4aceefbbb25571
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 19:25:22 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,3 +7,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 with request.Request(url) as response:
     html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
