396caa37e2575e8431d265e02af354523eeac19f
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 20:30:58 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value of the"""
+"""Takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and finally displays the body of the response."""
 
 import requests
 import sys
