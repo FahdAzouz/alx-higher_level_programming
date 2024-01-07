1ea15e0030dd91c30357ecb46c711b0dba911604
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 20:11:42 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
 
-from urllib import requests
+import requests
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
