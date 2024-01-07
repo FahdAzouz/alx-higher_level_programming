fe1ab0837bcfbfaa0b2102cd70adb93ce45541d8
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 20:15:05 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
