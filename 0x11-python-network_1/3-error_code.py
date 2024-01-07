d753a487413b04671115f94cfa2631c3073c9755
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 20:03:36 +0100

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email address, sends a POST request to the passed URL"""
+
+if __name__ == "__main__":
+    from urllib import request, error
+    import sys
+
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
