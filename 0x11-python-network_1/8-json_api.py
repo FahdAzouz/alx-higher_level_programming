9061f2b16810debb9af357591c0bf8b3a7b70c85
FahdAzouz <fahdstudies00@gmail.com>
2024-01-07 21:04:37 +0100

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,7 @@
 0.0.0.0:5000/search_user with the letter as a parameter."""
 import requests
 import sys
+
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     q = sys.argv[1] if len(sys.argv) > 1 else ""
@@ -12,4 +13,5 @@ if __name__ == "__main__":
     elif response.json() == {}:
         print('No result')
     else:
-        print('[{}] {}'.format(response.json().get('id'), response.json().get('name')))
+        print('[{}] {}'.format(response.json().get('id'),
+                               response.json().get('name')))
