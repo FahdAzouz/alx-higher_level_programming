2c431954955d5fd41227f69d1b20d045e3b51e2f
FahdAzouz <fahdstudies00@gmail.com>
2024-01-30 18:00:14 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const { FILE } = require('dns');
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const FilmId = process.argv[2];
@@ -8,7 +7,7 @@ const FilmId = process.argv[2];
 request(url + FilmId, (err, response, data) => {
   if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const body = JSON.parse(data);
     console.log(body.title);
   } else {
