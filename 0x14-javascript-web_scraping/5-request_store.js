0f44f5e4087f294c56a3a20c78723e5e71fa4b32
FahdAzouz <fahdstudies00@gmail.com>
2024-01-30 18:39:11 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const { request } = require('http');
-
-fs = require('fs');
-request = require('request');
+const fs = require('fs');
+const request = require('request');
 
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
